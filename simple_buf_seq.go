cbdb958305d59442051e18c09dd64474e952e6fb
cmc <cr.rey.clement@gmail.com>
2016-05-13 03:06:27 +0200

    SimpleBufSeq: fixed possible sync issues

--- simple_buf_seq.go
+++ simple_buf_seq.go
@@ -1,5 +1,7 @@
 package seq
 
+import "sync"
+
 // -----------------------------------------------------------------------------
 
 // SimpleBufSeq implements a simple buffered `Sequencer` backed by a local,
@@ -8,7 +10,12 @@ package seq
 // A `SimpleBufSeq` is not particularly useful in and of itself; but it provides
 // a good performance baseline that can be used as a point of comparison for
 // more complex implementations.
-type SimpleBufSeq chan ID
+type SimpleBufSeq struct {
+	ids chan ID
+
+	stop chan struct{}
+	wg   *sync.WaitGroup
+}
 
 // NewSimpleBufSeq returns a new `SimpleBufSeq` with the specified buffer size.
 //
@@ -16,32 +23,41 @@ type SimpleBufSeq chan ID
 // using a dedicated background routine.
 //
 // `bufSize` will default to 0 in case it is < 0.
-func NewSimpleBufSeq(bufSize int) SimpleBufSeq {
+func NewSimpleBufSeq(bufSize int) *SimpleBufSeq {
 	if bufSize < 0 {
 		bufSize = 0
 	}
 
-	seq := make(SimpleBufSeq, bufSize)
+	ids := make(chan ID, bufSize)
+	stop := make(chan struct{}, 0)
 	id := ID(1)
 
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
 	go func() { // background buffering routine
+		defer wg.Done()
 		for {
 			select {
-			case seq <- id: // blocks once `seq` is full
+			case ids <- id: // blocks once `seq` is full
 				id++
-			case <-seq: // stream has been closed, kill routine
+			case <-stop: // stream has been closed, kill routine
 				return
 			}
 		}
 	}()
 
-	return seq
+	return &SimpleBufSeq{ids: ids, stop: stop, wg: wg}
 }
 
 // GetStream returns a pre-buffered, range-able stream of monotonically
 // increasing IDs, starting at 1.
-func (ss SimpleBufSeq) GetStream() IDStream { return IDStream(chan ID(ss)) }
+func (ss SimpleBufSeq) GetStream() IDStream { return ss.ids }
 
 // Close closes the associated `IDStream`.
 // It always returns `nil`.
-func (ss SimpleBufSeq) Close() error { close(ss); return nil }
+func (ss *SimpleBufSeq) Close() error {
+	close(ss.stop) // kill background routine
+	ss.wg.Wait()
+	close(ss.ids) // close `ID` stream
+	return nil
+}

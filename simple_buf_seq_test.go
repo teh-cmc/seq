54e341fed1e6a199aa8b2fae5a706bbb9aadb304
cmc <cr.rey.clement@gmail.com>
2016-05-13 02:13:37 +0200

    TestSimpleBufSeq_SingleClient

--- simple_buf_seq_test.go
+++ simple_buf_seq_test.go
@@ -2,6 +2,7 @@ package seq
 
 import (
 	"testing"
+	"time"
 
 	"github.com/facebookgo/ensure"
 )
@@ -17,4 +18,19 @@ func TestSimpleBufSeq_New_BufSize(t *testing.T) {
 
 func TestSimpleBufSeq_FirstID(t *testing.T) {
 	ensure.DeepEqual(t, <-NewSimpleBufSeq(1e2).GetStream(), ID(1))
+}
+
+func TestSimpleBufSeq_SingleClient(t *testing.T) {
+	seq := NewSimpleBufSeq(1024)
+	lastID := ID(0)
+
+	go func() {
+		<-time.After(time.Millisecond * 250)
+		_ = seq.Close()
+	}()
+
+	for id := range seq.GetStream() {
+		ensure.DeepEqual(t, id, lastID+1)
+		lastID = id
+	}
 }

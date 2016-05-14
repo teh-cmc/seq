86596b5d295acd2aa7bc66a1c01103f0c18d697c
cmc <cr.rey.clement@gmail.com>
2016-05-14 07:45:08 +0200

    added TestRRSeq_FirstID

--- rr_seq/rr_seq_test.go
+++ rr_seq/rr_seq_test.go
@@ -4,67 +4,20 @@ import (
 	"testing"
 
 	"github.com/facebookgo/ensure"
+	"github.com/teh-cmc/seq"
 )
-
-// -----------------------------------------------------------------------------
-
-const (
-	testingRRSeqName = "idseq"
-
-	testingRRServer1Addr = ":16001"
-	testingRRServer2Addr = ":16002"
-	testingRRServer3Addr = ":16003"
-)
-
-var testingRRServerAddrs = []string{
-	testingRRServer1Addr,
-	testingRRServer2Addr,
-	testingRRServer3Addr,
-}
-
-var (
-	testingRRServer1 *RRServer
-	testingRRServer2 *RRServer
-	testingRRServer3 *RRServer
-)
-
-func init() {
-	var err error
-
-	go func() {
-		testingRRServer1, err = NewRRServer(testingRRServer1Addr,
-			testingRRServer2Addr,
-			testingRRServer3Addr,
-		)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	go func() {
-		testingRRServer2, err = NewRRServer(testingRRServer2Addr,
-			testingRRServer1Addr,
-			testingRRServer3Addr,
-		)
-		if err != nil {
-			panic(err)
-		}
-	}()
-
-	go func() {
-		testingRRServer3, err = NewRRServer(testingRRServer3Addr,
-			testingRRServer1Addr,
-			testingRRServer2Addr,
-		)
-		if err != nil {
-			panic(err)
-		}
-	}()
-}
 
 // -----------------------------------------------------------------------------
 
 // NOTE: run these tests with `go test -race -cpu 1,8,32`
+
+func TestRRSeq_FirstID(t *testing.T) {
+	rrseq, err := NewRRSeq(testingRRSeqName, 1e2, testingRRServerAddrs...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ensure.DeepEqual(t, <-rrseq.GetStream(), seq.ID(1))
+}
 
 func TestRRSeq_New_BufSize(t *testing.T) {
 	var rrseq *RRSeq

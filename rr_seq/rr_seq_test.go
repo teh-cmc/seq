bb545e9d891d6935f47885c9d5a5389692c9aca4
cmc <cr.rey.clement@gmail.com>
2016-05-14 07:27:04 +0200

    added TestRRSeq_New_BufSize

--- rr_seq/rr_seq_test.go
+++ rr_seq/rr_seq_test.go
@@ -1,5 +1,11 @@
 package rrs
 
+import (
+	"testing"
+
+	"github.com/facebookgo/ensure"
+)
+
 // -----------------------------------------------------------------------------
 
 const (
@@ -54,4 +60,37 @@ func init() {
 			panic(err)
 		}
 	}()
+}
+
+// -----------------------------------------------------------------------------
+
+// NOTE: run these tests with `go test -race -cpu 1,8,32`
+
+func TestRRSeq_New_BufSize(t *testing.T) {
+	var rrseq *RRSeq
+	var err error
+
+	rrseq, err = NewRRSeq(testingRRSeqName, -42, testingRRServerAddrs...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ensure.DeepEqual(t, cap(rrseq.ids), 0)
+
+	rrseq, err = NewRRSeq(testingRRSeqName, 0, testingRRServerAddrs...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ensure.DeepEqual(t, cap(rrseq.ids), 0)
+
+	rrseq, err = NewRRSeq(testingRRSeqName, 1, testingRRServerAddrs...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ensure.DeepEqual(t, cap(rrseq.ids), 1)
+
+	rrseq, err = NewRRSeq(testingRRSeqName, 1e6, testingRRServerAddrs...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ensure.DeepEqual(t, cap(rrseq.ids), int(1e6))
 }

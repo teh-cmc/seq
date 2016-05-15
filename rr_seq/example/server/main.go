2b1eaaafa033edbe3cdc99ab7625adda04e62751
cmc <cr.rey.clement@gmail.com>
2016-05-15 21:17:38 +0200

    RRSeq serv example: added mandatory path param

--- rr_seq/example/server/main.go
+++ rr_seq/example/server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
@@ -15,23 +14,16 @@ import (
 
 var s *rrs.RRServer
 
-func usage() string { return "./server addr peer1_addr [peerN_addr...]" }
+func usage() string { return "./server addr path peer1_addr [peerN_addr...]" }
 
 func main() {
 	flag.Parse()
-	if flag.NArg() < 1 {
+	if flag.NArg() < 3 {
 		fmt.Println("Usage:", usage())
 		os.Exit(1)
 	}
 
-	f, err := ioutil.TempFile("", "server_example")
-	if err != nil {
-		log.Fatal(err)
-	}
-	path := f.Name()
-	_ = f.Close()
-
-	s, err = rrs.NewRRServer(flag.Arg(0), path, flag.Args()[1:]...)
+	s, err := rrs.NewRRServer(flag.Arg(0), flag.Arg(1), flag.Args()[2:]...)
 	log.Println("ready")
 
 	c := make(chan os.Signal, 1)
